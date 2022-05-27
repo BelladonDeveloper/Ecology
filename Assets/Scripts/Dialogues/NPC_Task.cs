1e94d4771f71688c08a7da817969f3e1c1322db9
EhKeule <104719622+EhKeule@users.noreply.github.com>
2022-05-27 21:18:29 +0300

    Затестил Диалоги,вроде все работает.Были проблемы с "InputValue",поэтому устновил ассеты дополнительные

---
+++ Assets/Scripts/Dialogues/NPC_Task.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Task : MonoBehaviour
+{
+    public bool end_Dialogue;
+    public GameObject dialogue;
+    public GameObject dialogue2;
+    public Quests QE;
+    public bool findial;
+    public GameObject particles;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(end_Dialogue == true)
+        {
+
+            Time.timeScale = 1;
+            QE.Quest1 = true;
+            dialogue.SetActive(false);
+            particles.SetActive(true);
+
+        }
+        if(findial == true)
+        {
+            Time.timeScale = 1;
+            QE.Quest1 = false;
+            dialogue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Time.timeScale = 0;
+            if(QE.endQuest1 == false)
+            {
+                dialogue.SetActive(true);
+            }
+            else
+            {
+                dialogue2.SetActive(true);
+            }
+            
+        }
+    }
+}
