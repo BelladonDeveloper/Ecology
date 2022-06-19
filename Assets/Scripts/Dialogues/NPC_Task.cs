b6a424717ecdab47eac1863c29e0c38d7cab79c4
Ihor Sichkar <99723162+IhorSichkar@users.noreply.github.com>
2022-06-19 15:02:59 +0300

    Add Player, Jody, MimiMap, DialogSystem to First Level scene

--- Assets/Scripts/Dialogues/NPC_Task.cs
+++ Assets/Scripts/Dialogues/NPC_Task.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NPC_Task : MonoBehaviour
 {
@@ -9,20 +7,14 @@ public class NPC_Task : MonoBehaviour
     public GameObject dialogue2;
     public Quests QE;
     public bool findial;
-    public GameObject particles;
     
-
-
-    // Update is called once per frame
     void Update()
     {
         if(end_Dialogue == true)
         {
-
             Time.timeScale = 1;
             QE.Quest1 = true;
             dialogue.SetActive(false);
-            particles.SetActive(true);
 
         }
         if(findial == true)
@@ -45,8 +37,7 @@ public class NPC_Task : MonoBehaviour
             else
             {
                 dialogue2.SetActive(true);
-            }
-            
+            }            
         }
     }
 }
