b15dd0e6ec435641e618a5add0ee21d2306f4820
EhKeule <104719622+EhKeule@users.noreply.github.com>
2022-07-24 14:31:29 +0300

    Создал скрипт для счётчика

---
+++ Assets/Scripts/Dialogues/Good_BadFoodCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Good_BadFoodCounter : MonoBehaviour
+{
+    [SerializeField] private sbyte s;
+    [SerializeField] private Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
