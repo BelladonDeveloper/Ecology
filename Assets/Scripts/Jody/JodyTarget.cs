a1f775b82e382e7d0440c3430034986ddbb216a7
MykolaievychDima <94140997+MykolaievychDima@users.noreply.github.com>
2022-07-31 13:57:37 +0200

    jody

--- Assets/Scripts/Jody/JodyTarget.cs
+++ Assets/Scripts/Jody/JodyTarget.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class JodyTarget : MonoBehaviour
 {
     public GameObject ZaxidOne;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
          Vector3 diff = ZaxidOne.transform.position - transform.position;
