adb0fc3fd7ef3155b3773912eb4df6dc8e2af53a
MykolaievychDima <94140997+MykolaievychDima@users.noreply.github.com>
2022-08-12 12:35:26 +0200

    Jodu Target

---
+++ Assets/Scripts/Jodu/JodyTarget.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JodyTarget : MonoBehaviour
+{
+     public GameObject ZaxidOne;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = transform.position - ZaxidOne.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(-diff);
+        transform.rotation = rotation;
+    }
+}
